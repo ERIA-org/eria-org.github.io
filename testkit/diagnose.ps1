d0ad78b2a11497c22caa7428567ed450bf56594f
Arnold Siboro <arnold@siboro.com>
2024-02-07 20:44:58 +0900

    Revision

--- testkit/diagnose.ps1
+++ testkit/diagnose.ps1
@@ -1,145 +1,145 @@
 $ScriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-'Test date:' > $ScriptPath\diagnose.txt
-Get-Date >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'IP info:' >> $ScriptPath\diagnose.txt
-Invoke-RestMethod -Method Get -Uri "http://ip-api.com/json/"  >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'PC info:' >> $ScriptPath\diagnose.txt
-Get-ComputerInfo  >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'CPU usage info:' >> $ScriptPath\diagnose.txt
-(Get-WmiObject -Class win32_processor -ErrorAction Stop | Measure-Object -Property LoadPercentage -Average | Select-Object Average).Average  >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'RAM usage:' >> $ScriptPath\diagnose.txt
-Get-WmiObject win32_OperatingSystem |%{"Total Physical Memory: {0}KB`nFree Physical Memory : {1}KB`nTotal Virtual Memory : {2}KB`nFree Virtual Memory  : {3}KB" -f $_.totalvisiblememorysize, $_.freephysicalmemory, $_.totalvirtualmemorysize, $_.freevirtualmemory}   >> $ScriptPath\diagnose.txt
+'Test date:' > $ScriptPath\result.txt
+Get-Date >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'IP info:' >> $ScriptPath\result.txt
+Invoke-RestMethod -Method Get -Uri "http://ip-api.com/json/"  >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'PC info:' >> $ScriptPath\result.txt
+Get-ComputerInfo  >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'CPU usage info:' >> $ScriptPath\result.txt
+(Get-WmiObject -Class win32_processor -ErrorAction Stop | Measure-Object -Property LoadPercentage -Average | Select-Object Average).Average  >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'RAM usage:' >> $ScriptPath\result.txt
+Get-WmiObject win32_OperatingSystem |%{"Total Physical Memory: {0}KB`nFree Physical Memory : {1}KB`nTotal Virtual Memory : {2}KB`nFree Virtual Memory  : {3}KB" -f $_.totalvisiblememorysize, $_.freephysicalmemory, $_.totalvirtualmemorysize, $_.freevirtualmemory}   >> $ScriptPath\result.txt
 
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
 
 ### Test local
-'1. LOCAL TESTS' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'1-1 Test ping connectivity to ERIA router' >> $ScriptPath\diagnose.txt
-Test-NetConnection -ComputerName "192.168.0.1" -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'1. LOCAL TESTS' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'1-1 Test ping connectivity to the router' >> $ScriptPath\result.txt
+Test-NetConnection -ComputerName "192.168.0.1" -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'1-2 Test ping connectivity to ERIA router with routing diagnosis' >> $ScriptPath\diagnose.txt
-Test-NetConnection -ComputerName "192.168.0.1" -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'1-2 Test ping connectivity to the router with routing diagnosis' >> $ScriptPath\result.txt
+Test-NetConnection -ComputerName "192.168.0.1" -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'1-3 Test TCP connectivity to ERIA router' >> $ScriptPath\diagnose.txt
-Test-NetConnection -Port 80 -ComputerName "192.168.0.1" -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
+'1-3 Test TCP connectivity to the router' >> $ScriptPath\result.txt
+Test-NetConnection -Port 80 -ComputerName "192.168.0.1" -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
 
 
 ### Test remote (ISP - Linknet)
-'2. REMOTE TESTS (ISP - LINKNET)' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'2-1 Test ping connectivity to www.linknet.co.id' >> $ScriptPath\diagnose.txt
-Test-NetConnection -ComputerName "www.linknet.co.id" -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'2. REMOTE TESTS (ISP - LINKNET)' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'2-1 Test ping connectivity to www.linknet.co.id' >> $ScriptPath\result.txt
+Test-NetConnection -ComputerName "www.linknet.co.id" -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'2-2 Test ping connectivity to www.linknet.co.id with routing diagnosis' >> $ScriptPath\diagnose.txt
-Test-NetConnection -ComputerName "www.linknet.co.id" -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'2-2 Test ping connectivity to www.linknet.co.id with routing diagnosis' >> $ScriptPath\result.txt
+Test-NetConnection -ComputerName "www.linknet.co.id" -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'2-3 Test TCP connectivity to www.linknet.co.id' >> $ScriptPath\diagnose.txt
-Test-NetConnection -Port 80 -ComputerName "www.linknet.co.id" -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
+'2-3 Test TCP connectivity to www.linknet.co.id' >> $ScriptPath\result.txt
+Test-NetConnection -Port 80 -ComputerName "www.linknet.co.id" -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
 
 
 
 ### Test remote (ISP - Astinet)
-'3. REMOTE TESTS (ISP - ASTINET)' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'3-1 Test ping connectivity to www.telkom.co.id' >> $ScriptPath\diagnose.txt
-Test-NetConnection -ComputerName "www.telkom.co.id" -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'3. REMOTE TESTS (ISP - ASTINET)' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'3-1 Test ping connectivity to www.telkom.co.id' >> $ScriptPath\result.txt
+Test-NetConnection -ComputerName "www.telkom.co.id" -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'3-2 Test ping connectivity to www.telkom.co.id with routing diagnosis' >> $ScriptPath\diagnose.txt
-Test-NetConnection -ComputerName "www.telkom.co.id" -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'3-2 Test ping connectivity to www.telkom.co.id with routing diagnosis' >> $ScriptPath\result.txt
+Test-NetConnection -ComputerName "www.telkom.co.id" -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'3-3 Test TCP connectivity to www.telkom.co.id' >> $ScriptPath\diagnose.txt
-Test-NetConnection -Port 80 -ComputerName "www.telkom.co.id" -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
+'3-3 Test TCP connectivity to www.telkom.co.id' >> $ScriptPath\result.txt
+Test-NetConnection -Port 80 -ComputerName "www.telkom.co.id" -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
 
 ### Test remote (ISP - Biznet)
-'4. REMOTE TESTS (ISP - BIZNET)' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'4-1 Test ping connectivity to www.biznetnetworks.com' >> $ScriptPath\diagnose.txt
-Test-NetConnection -ComputerName "www.biznetnetworks.com" -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'4. REMOTE TESTS (ISP - BIZNET)' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'4-1 Test ping connectivity to www.biznetnetworks.com' >> $ScriptPath\result.txt
+Test-NetConnection -ComputerName "www.biznetnetworks.com" -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'4-2 Test ping connectivity to www.biznetnetworks.com with routing diagnosis' >> $ScriptPath\diagnose.txt
-Test-NetConnection -ComputerName "www.biznetnetworks.com" -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'4-2 Test ping connectivity to www.biznetnetworks.com with routing diagnosis' >> $ScriptPath\result.txt
+Test-NetConnection -ComputerName "www.biznetnetworks.com" -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'4-3 Test TCP connectivity to www.biznetnetworks.com' >> $ScriptPath\diagnose.txt
-Test-NetConnection -Port 80 -ComputerName "www.biznetnetworks.com" -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
+'4-3 Test TCP connectivity to www.biznetnetworks.com' >> $ScriptPath\result.txt
+Test-NetConnection -Port 80 -ComputerName "www.biznetnetworks.com" -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
 
 
 ### Test remote (domestic)
-'5. REMOTE TESTS (DOMESTIC)' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'5-1 Test ping connectivity to www.detik.com' >> $ScriptPath\diagnose.txt
-Test-NetConnection -ComputerName "www.detik.com" -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'5. REMOTE TESTS (DOMESTIC)' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'5-1 Test ping connectivity to www.detik.com' >> $ScriptPath\result.txt
+Test-NetConnection -ComputerName "www.detik.com" -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'5-2 Test ping connectivity to www.detik.com with routing diagnosis' >> $ScriptPath\diagnose.txt
-Test-NetConnection -ComputerName "www.detik.com" -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'5-2 Test ping connectivity to www.detik.com with routing diagnosis' >> $ScriptPath\result.txt
+Test-NetConnection -ComputerName "www.detik.com" -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'5-3 Test TCP connectivity to www.detik.com' >> $ScriptPath\diagnose.txt
-Test-NetConnection -Port 80 -ComputerName "www.detik.com" -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
+'5-3 Test TCP connectivity to www.detik.com' >> $ScriptPath\result.txt
+Test-NetConnection -Port 80 -ComputerName "www.detik.com" -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
 
 ### Test remote (global)
-'6. REMOTE TESTS (GLOBAL)' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
-'6-1 Test ping connectivity to Microsoft' >> $ScriptPath\diagnose.txt
-Test-NetConnection -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt 
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
+'6. REMOTE TESTS (GLOBAL)' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
+'6-1 Test ping connectivity to Microsoft' >> $ScriptPath\result.txt
+Test-NetConnection -InformationLevel "Detailed" >> $ScriptPath\result.txt 
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
 
-'6-2 Test ping connectivity to Microsoft with routing diagnosis' >> $ScriptPath\diagnose.txt
-Test-NetConnection -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt 
+'6-2 Test ping connectivity to Microsoft with routing diagnosis' >> $ScriptPath\result.txt
+Test-NetConnection -DiagnoseRouting -InformationLevel "Detailed" >> $ScriptPath\result.txt
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt 
 
-'6-3 Test TCP connectivity to Microsoft' >> $ScriptPath\diagnose.txt
-Test-NetConnection -Port 80 -InformationLevel "Detailed" >> $ScriptPath\diagnose.txt 
-'---------------------------------------------' >> $ScriptPath\diagnose.txt
-'' >> $ScriptPath\diagnose.txt
+'6-3 Test TCP connectivity to Microsoft' >> $ScriptPath\result.txt
+Test-NetConnection -Port 80 -InformationLevel "Detailed" >> $ScriptPath\result.txt 
+'---------------------------------------------' >> $ScriptPath\result.txt
+'' >> $ScriptPath\result.txt
 
 
 
